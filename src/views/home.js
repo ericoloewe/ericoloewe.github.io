a332b065910d38f188692ba1300d41fb491b2e0d
Érico de Souza Loewe <ed-lowe@hotmail.com>
2018-05-26 16:55:27 -0300

    feat(home): add home implementation

--- src/views/home.js
+++ src/views/home.js
@@ -7,7 +7,18 @@ import { LayoutView } from './shared/layout';
 export default class Home extends React.Component {
   render() {
     return (
-      <LayoutView className="container home" customBottom={<script type="text/javascript" src="~scripts/home.bundle.js" />} customHead={<link rel="stylesheet" type="text/css" href="~styles/home.css" />}>
+      <LayoutView
+        className="container home"
+        customBottom={<script type="text/javascript" src="~scripts/home.bundle.js" />}
+        customHead={
+          <React.Fragment>
+            <title>Home - Érico de Souza Loewe</title>
+            <meta name="author" content="Érico de Souza Loewe" />
+            <meta name="description" content="A page about me" />
+            <meta name="keywords" content="Érico,Loewe,About,Me,HTML,CSS,JavaScript" />
+            <link rel="stylesheet" type="text/css" href="~styles/home.css" />
+          </React.Fragment>
+        }>
         <div className="profile-picture">
           <img src='~images/profile.jpg' alt="My profile photo" />
         </div>
