70eea142491205c4743132d956bff272eaca74b2
Erico de Souza Loewe <erico.loewe@dbccompany.com.br>
2024-04-29 10:20:34 -0300

    new content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,47 +5,53 @@ import "./page.scss";
 import { Layout } from "./shared/layout";
 import { Links } from "./components/links";
 
+const topics = [
+  {
+    title: <h5>🎵 Apaixonado por Música e Violão 🎸</h5>,
+    description: [
+      <p>Sou um entusiasta da música e dedico meu tempo livre ao violão. 🎶</p>,
+      <p>Desenvolvi um <a href="https://ericoloewe.github.io/computer-science-tcc/" target="_blank">plugin de recomendação para o Spotify</a>, tornando a experiência musical ainda mais personalizada. 🎧</p>,
+    ],
+  },
+  {
+    title: <h5>🛹 Aventureiro e Surfista 🏄🏽‍♂️</h5>,
+    description: [
+      <p>Adoro aventuras e tenho um longboard para explorar novos lugares. 🌊</p>,
+      <p>Meu amor pelo surf me conecta com a natureza e me inspira. 🌴</p>,
+    ],
+  },
+  {
+    title: <h5>👨‍💻 Desenvolvedor de Software na <a href="https://www.dbccompany.com.br/" target="_blank" rel="noopener noreferrer">DBC Company</a></h5>,
+    description: [
+      <p>Trabalho como desenvolvedor de software na <a href="https://www.dbccompany.com.br/" target="_blank" rel="noopener noreferrer">DBC Company</a>, onde aplico minha paixão pela programação. 💻</p>,
+      <p>Formado em Ciência da Computação pela <a href="https://www.feevale.br/" target="_blank" rel="noopener noreferrer">Universidade Feevale</a>, tenho uma base sólida em teoria e prática. 🎓</p>,
+    ],
+  },
+  {
+    title: <h5>🏢 Fundador da <a href="https://loewesolucoes.github.io/" target="_blank">@loewe.solucoes</a></h5>,
+    description: [
+      <p>Fundei a <a href="https://loewesolucoes.github.io/" target="_blank">@loewe.solucoes</a>, onde aplico minha criatividade e habilidades técnicas para resolver problemas. 💡</p>,
+      <p>Também criei um jogo <a href="https://ericoloewe.github.io/snake/" target="_blank" rel="noopener noreferrer">“snake”</a> como projeto pessoal. 🐍</p>,
+    ],
+  },
+]
+
 function Home() {
   return (
-    <div className="main">
-      <main className={`main home p5`}>
-        <div className="profile_info">
-          <h1 className="name">Érico de Souza Loewe</h1>
-          <p className="description">
-            🎵 Amo musica <br />
-            🎸 Amo meu violão <br />
-            🛹 Amo aventura e tenho um long <br />
-            🏄🏽‍♂️ Amo surf <br />
-            👨‍💻 Software Developer na{" "}
-            <a href="https://www.dbccompany.com.br/" target="_blank" rel="noopener noreferrer">
-              DBC Company
-            </a>{" "}
-            <br />
-            👨‍💻 Formado em Ciência da Computação na{" "}
-            <a href="https://www.feevale.br/" target="_blank" rel="noopener noreferrer">
-              Universidade Feevale
-            </a>{" "}
-            <br />
-            👨‍💻 Desenvolveu um{" "}
-            <a href="https://ericoloewe.github.io/computer-science-tcc/" target="_blank">
-              plugin de recomendação para o Spotify
-            </a>{" "}
-            <br />
-            👨‍💻 Desenvolveu um{" "}
-            <a href="https://ericoloewe.github.io/snake/" target="_blank" rel="noopener noreferrer">
-              snake game
-            </a>{" "}
-            <br />
-            🏢 Fundador do {" "}
-            <a href="https://loewesolucoes.github.io/" target="_blank">
-              @loewe.solucoes
-            </a>{" "}
-            <br />
-          </p>
-        </div>
-        <Links />
-      </main>
-    </div>
+    <main className={`py-5 px-2`}>
+      <div className="profile_info">
+        <h1 className="name mb-3">Érico de Souza Loewe</h1>
+        <ul>
+          {topics.map(x => (
+            <li key={x.title}>
+              {x.title}
+              {x.description}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Links />
+    </main>
   );
 
 }
@@ -54,10 +60,10 @@ export default function Page() {
 
   return (
     <>
-      <img className="background" src="./erico-loewe.jpg" alt="Surfista Érico de Souza Loewe na praia" />
       <Layout>
         <Home />
       </Layout>
+      <img className="background" src="./erico-loewe.jpg" alt="Surfista Érico de Souza Loewe na praia" />
     </>
   );
 }
